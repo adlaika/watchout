5922ddb7d4cb23034ff6fbc105d3780f166303b8
Hack Reactor Students <accounts@hackreactor.com>
2015-05-23 11:18:53 -0700

    Detect when player and enemy collide

--- watchout.js
+++ watchout.js
@@ -22,8 +22,8 @@
 
   // Appends the SVG board to the container element where gameplay happens
   var gameBoard = d3.select('.container').append('svg')
-    .attr('width', gameOptions.width)
-    .attr('height', gameOptions.height);
+  .attr('width', gameOptions.width)
+  .attr('height', gameOptions.height);
 
   var GameElement = function(x, y){
     this.x = x;
@@ -31,32 +31,32 @@
   };
 
   //Player Construction
-  var Player = function(x, y) {
-    GameElement.apply(this, arguments);
-    this.id = 'player';
-    this.fill = '#ff6600';
-    this.r = 8;
-  }
+  // var Player = function(x, y) {
+  //   GameElement.apply(this, arguments);
+  //   this.id = 'player';
+  //   this.fill = '#ff6600';
+  //   this.r = 8;
+  // }
 
-  Player.prototype = Object.create(GameElement.prototype);
-  Player.prototype.constructor = Player;
+  // Player.prototype = Object.create(GameElement.prototype);
+  // Player.prototype.constructor = Player;
 
   var drag = d3.behavior.drag()
-               .on('dragstart', function() { player.style('fill', 'red'); })
-               .on('drag', function() { player.attr('cx', d3.event.x)
-                                              .attr('cy', d3.event.y); })
-               .on('dragend', function() { player.style('fill', 'purple'); });
+    .on('dragstart', function() { player.style('fill', 'red'); })
+    .on('drag', function() { player.attr('cx', d3.event.x)
+      .attr('cy', d3.event.y); })
+    .on('dragend', function() { player.style('fill', 'purple'); });
 
   var player = gameBoard.selectAll('.draggablePlayer')
-                .data([{ x: (gameOptions.width / 2), y: (gameOptions.height / 2), r: 8 }])
-                .enter()
-                .append('svg:circle')
-                .attr('class', 'draggablePlayer')
-                .attr('cx', function(d) { return d.x; })
-                .attr('cy', function(d) { return d.y; })
-                .attr('r', function(d) { return d.r; })
-                .call(drag)
-                .style('fill', 'purple');
+    .data([{ x: (gameOptions.width / 2), y: (gameOptions.height / 2), r: 10 }])
+    .enter()
+    .append('svg:circle')
+    .attr('class', 'draggablePlayer')
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .attr('r', function(d) { return d.r; })
+    .call(drag)
+    .style('fill', 'purple');
 
   //Enemy Construction
   var Enemy = function(x, y, id){
@@ -76,7 +76,6 @@
   };
 
   var render = function(enemy_data){
-    console.log(enemy_data);
     // Associates enemy data set with their svg elements
     var enemies = gameBoard.selectAll('circle.enemy')
     .data( enemy_data, function(d){ return d.id } );
@@ -94,6 +93,23 @@
     enemies.exit()
     .remove();
 
+    // Detects if distance between centers is greater than combined radii
+    var checkCollision = function (enemy, collidedCallback){
+      var radiusSum = parseFloat(enemy.attr('r')) + parseFloat(player.attr('r'));
+      var xDiff = parseFloat(enemy.attr('cx')) - parseFloat(player.attr('cx'));
+      var yDiff = parseFloat(enemy.attr('cy')) - parseFloat(player.attr('cy'));
+
+      var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2));
+      // debugger;
+      if (separation < radiusSum) {
+        collidedCallback(player, enemy);
+      }
+    };
+
+    var onCollision = function(){
+      console.log('ouch');
+    }
+
     // Handles frame by frame animation
     var tweenMagic = function(data) {
       var enemy = d3.select(this);
@@ -109,6 +125,8 @@
       };
 
       return function(t) {
+        checkCollision(enemy, onCollision);
+
         // Increments frame position based on difference of x or y over time
         var enemyNextPos = {
           x: startPosition.x + (endPosition.x - startPosition.x)*t,
